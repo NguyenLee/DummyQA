0d1de8d218ced2ca36785f3308066a8b337e60c3
NguyenLee <nguyen.le@webflexica.com>
2017-02-22 13:56:59 +0700

    Default theme

---
+++ themes/default/js/badge_code_v2.js
@@ -0,0 +1,27 @@
+var b_txt = '';
+
+// write the badge
+	
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="flickr_badge_image1"><a href="https://www.flickr.com/photos/chapter3/3221707896/"><img src="https://farm4.staticflickr.com/3311/3221707896_54893861e8_s.jpg" alt="A photo on Flickr" title="2005 August - NYC views" height="75" width="75"></a></div>';
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="flickr_badge_image2"><a href="https://www.flickr.com/photos/chapter3/3221704130/"><img src="https://farm4.staticflickr.com/3387/3221704130_31953201cd_s.jpg" alt="A photo on Flickr" title="2005 August - NYC views" height="75" width="75"></a></div>';
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="flickr_badge_image3"><a href="https://www.flickr.com/photos/chapter3/3221702350/"><img src="https://farm4.staticflickr.com/3362/3221702350_b9f44ffba2_s.jpg" alt="A photo on Flickr" title="2005 August - NYC views" height="75" width="75"></a></div>';
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="flickr_badge_image4"><a href="https://www.flickr.com/photos/chapter3/3221701778/"><img src="https://farm4.staticflickr.com/3305/3221701778_aa494eb761_s.jpg" alt="A photo on Flickr" title="2005 August - NYC views" height="75" width="75"></a></div>';
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="flickr_badge_image5"><a href="https://www.flickr.com/photos/chapter3/3221701148/"><img src="https://farm4.staticflickr.com/3317/3221701148_d700e1582e_s.jpg" alt="A photo on Flickr" title="2005 August - NYC views" height="75" width="75"></a></div>';
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="flickr_badge_image6"><a href="https://www.flickr.com/photos/chapter3/3221699962/"><img src="https://farm4.staticflickr.com/3418/3221699962_e619a5f7f6_s.jpg" alt="A photo on Flickr" title="2005 August - NYC views" height="75" width="75"></a></div>';
+
+
+b_txt += '<span style="position:absolute;left:-999em;top:-999em;visibility:hidden" class="flickr_badge_beacon"><img src="https://geo.yahoo.com/p?s=792600102&t=c5279d03b7641e730574cb957d7ec9bf&r=http%3A%2F%2Fuacademy.html.themeforest.createit.pl%2Findex.html&fl_ev=0&lang=en&intl=sg" width="0" height="0" alt="" /></span>';
+
+document.write(b_txt);
